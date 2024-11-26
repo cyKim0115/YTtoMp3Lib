11735b3f6a800b9dc1ee984c4c084c19d0d9ceca
cyKim <76chanyoung@gmail.com>
2024-11-26 11:22:02 +0900

    최초 커밋, version 1.0.0

---
+++ YDProjectClassLibrary/ConvertRequestInfo.cs
@@ -0,0 +1,16 @@
+﻿
+namespace YDLib
+{
+    public class ConvertRequestInfo
+    {
+        public string name;
+        public string url;
+        public string extension = ".mp3";
+    }
+
+    public class DownloadRequestInfo
+    {
+        public string name;
+        public string extension = ".mp3";
+    }
+}
