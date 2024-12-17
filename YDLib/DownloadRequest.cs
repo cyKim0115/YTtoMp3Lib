b8b61423b39e93ce80db0470de06461b6983fdf8
cyKim <76chanyoung@gmail.com>
2024-12-17 17:08:30 +0900

    사용되지 않는 DownloadRequest삭제 및 버전 업

--- YDLib/DownloadRequest.cs
+++
@@ -1,9 +0,0 @@
-
-// ReSharper disable CheckNamespace
-namespace YDLib;
-
-public class DownloadRequest
-{
-    public string name { get; set; } = "";
-    public string extension { get; set; } = ".mp3";
-}
